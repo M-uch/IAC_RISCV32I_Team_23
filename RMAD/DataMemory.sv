8018840c8f7548d7363d36f683ed1ffb08be146d
MatthewGodsmark <mg1322@ic.ac.uk>
2023-12-01 12:02:20 +0000

    new 3 input Mux for PCPlus4 and error checking

--- RMAD/DataMemory.sv
+++ RMAD/DataMemory.sv
@@ -1,15 +1,14 @@
 module DataMemory #(
-    parameter   ADDRESS_WIDTH = 5,
-                DATA_WIDTH = 32
+    parameter   DATA_WIDTH = 32
 )(
     input   logic                       clk,
-    input   logic [ADDRESS_WIDTH-1:0]   A,
+    input   logic [DATA_WIDTH-1:0]      A,
     input   logic [DATA_WIDTH-1:0]      WD,
     input   logic                       WE,
-    output  logic [DATA_WIDTH-1:0]      RD,
+    output  logic [DATA_WIDTH-1:0]      RD
 );
 
-logic   [DATA_WIDTH-1:0] Reg_File [2**ADDRESS_WIDTH-1:0];
+logic   [DATA_WIDTH-1:0] Reg_File [2**DATA_WIDTH-1:0];
 
 always_ff @(posedge clk) begin
     if(WE) Reg_File[A] <= WD;
