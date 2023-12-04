9d775dc97444dafa5005c65c5b1cf6505906f37f
AlexSeferidis <alex.seferidis@gmail.com>
2023-12-04 20:31:24 +0000

    added jump_src

--- CU/main_decoder.sv
+++ CU/main_decoder.sv
@@ -11,7 +11,8 @@ module main_decoder #(
     output logic alu_src,
     output logic [3_WIDTH-1:0] imm_src,
     output logic reg_write,
-    output logic [3_WIDTH-1:0] alu_op
+    output logic [3_WIDTH-1:0] alu_op,
+    output logic               jump_src
 );
 
 typedef enum logic [6:0] {
@@ -27,7 +28,7 @@ typedef enum logic [6:0] {
 } Instruction_Type;
 
 // Define Control Code
-assign {reg_write, imm_src, alu_src, mem_write, result_src, branch, alu_op, jump} = command_code; // takes form x_xxx_x_x_xx_x_xxx_x
+assign {reg_write, imm_src, alu_src, mem_write, result_src, branch, alu_op, jump, jump_src} = command_code; // takes form x_xxx_x_x_xx_x_xxx_x_x
 
 always_comb begin
     Instruction_Type opcode = op;
@@ -36,16 +37,16 @@ always_comb begin
         // NOTE: 
         // - not sure about result_src for Upper instructions
         // - alu_op is same for R and I_ALU Instruction types
-        Type_R:         command_code = 13'b1_xxx_0_0_00_0_000_0 ;
-        Type_I:         command_code = 13'b1_001_1_0_01_0_001_0 ;
-        Type_I_ALU:     command_code = 13'b1_001_1_0_00_0_000_0 ;
-        Type_S:         command_code = 13'b0_011_1_1_xx_0_010_0 ;
-        Type_B:         command_code = 13'b0_100_0_0_xx_1_011_0 ;
-        Type_U:         command_code = 13'b1_010_1_0_00_0_100_0 ;
-        Type_U_LUI:     command_code = 13'b1_010_1_0_00_0_101_0 ;
-        Type_J_JALR:    command_code = 13'b1_101_0_0_10_0_110_1 ;
-        Type_J_JAL:     command_code = 13'b1_101_0_0_10_0_111_1 ;
-        default:        command_code = 13'bx_xxx_x_x_xx_x_xxx_x ;
+        Type_R:         command_code = 13'b1_xxx_0_0_00_0_000_0_x ;
+        Type_I:         command_code = 13'b1_001_1_0_01_0_001_0_x ;
+        Type_I_ALU:     command_code = 13'b1_001_1_0_00_0_000_0_x ;
+        Type_S:         command_code = 13'b0_011_1_1_xx_0_010_0_x ;
+        Type_B:         command_code = 13'b0_100_0_0_xx_1_011_0_x ;
+        Type_U:         command_code = 13'b1_010_1_0_00_0_100_0_x ;
+        Type_U_LUI:     command_code = 13'b1_010_1_0_00_0_101_0_x ;
+        Type_J_JALR:    command_code = 13'b1_101_0_0_10_0_110_1_1 ;
+        Type_J_JAL:     command_code = 13'b1_101_0_0_10_0_111_1_0 ;
+        default:        command_code = 13'bx_xxx_x_x_xx_x_xxx_x_x ;
 
     endcase
 end
