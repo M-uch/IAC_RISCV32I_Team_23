f4e4aa5e22b2200941f48a4251f6dbfc5ed506da
M-uch <Raymondla2004@gmail.com>
2023-12-05 10:56:35 +0000

    designated control signals to imm extend

--- ImmExtend/signextend.sv
+++ ImmExtend/signextend.sv
@@ -15,11 +15,11 @@ always_comb begin
 if (code[31]) extend = 'hFFFFF;
 else extend = 'h00000;
 
-if (immscr==3'b000) immop = {extend, code[31:20]}; // I type  
-else if (immscr==3'b001) immop = {32'b0}; // U type UNUSED
-else if (immscr==3'b001) immop = {extend, code[31:25], code[11:7] }; // S type
-else if (immscr==3'b001) immop = {extend[18:0], code[31], code[7], code[30:25], code[11:8], 1'b0}; // B type 
-else if (immscr==3'b001) immop = {extend[10:0], code[31], code[19:12], code[20], code[30:21], 1'b0}; // J type 
+if (immscr==3'b001) immop = {extend, code[31:20]}; // I type  
+else if (immscr==3'b010) immop = {32'b0}; // U type UNUSED
+else if (immscr==3'b011) immop = {extend, code[31:25], code[11:7] }; // S type
+else if (immscr==3'b100) immop = {extend[18:0], code[31], code[7], code[30:25], code[11:8], 1'b0}; // B type 
+else if (immscr==3'b101) immop = {extend[10:0], code[31], code[19:12], code[20], code[30:21], 1'b0}; // J type 
 
 else immop = 32'b0; // default case
 
