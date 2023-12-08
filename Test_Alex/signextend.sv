5cf44df9f9857f135d067f0629adbd5f6a7b1d81
M-uch <Raymondla2004@gmail.com>
2023-12-08 15:11:40 +0000

    added U type imm extend

--- Test_Alex/signextend.sv
+++ Test_Alex/signextend.sv
@@ -16,7 +16,7 @@ if (code[31]) extend = 'hFFFFF;
 else extend = 'h00000;
 
 if (immscr==3'b001) immop = {extend, code[31:20]}; // I type  
-else if (immscr==3'b010) immop = {32'b0}; // U type UNUSED
+else if (immscr==3'b010) immop = {code[31:12], 12'b0}; // U type 
 else if (immscr==3'b011) immop = {extend, code[31:25], code[11:7] }; // S type
 else if (immscr==3'b100) immop = {extend[18:0], code[31], code[7], code[30:25], code[11:8], 1'b0}; // B type 
 else if (immscr==3'b101) immop = {extend[10:0], code[31], code[19:12], code[20], code[30:21], 1'b0}; // J type 
