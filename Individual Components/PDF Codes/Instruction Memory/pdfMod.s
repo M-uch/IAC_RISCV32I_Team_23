5d905c62f34b0d278039cf43b9c5a563e79f8fbe
M-uch <Raymondla2004@gmail.com>
2023-12-10 21:58:10 +0000

    program modification

--- Individual Components/PDF Codes/Instruction Memory/pdfMod.s
+++ Individual Components/PDF Codes/Instruction Memory/pdfMod.s
@@ -9,8 +9,8 @@ forever:
     JAL     ra, display
     J       forever
 
-init:       # function to initialise PDF buffer memory 
-    LI      a1, 0x100           # loop_count a1 = 256
+init:       # function to initialise PDF buffer memory WIPES PDF ARRAY TO ALL 0
+    LI      a1, 0xff           # loop_count a1 = 256
 _loop1:                         # repeat
     ADDI    a1, a1, -1          #     decrement a1
     SB      zero, base_pdf(a1)  #     mem[base_pdf+a1) = 0
