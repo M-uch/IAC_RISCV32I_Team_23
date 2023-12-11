861bf4e906655ee4075c6343a6e7eac0e45373ab
MatthewGodsmark <mg1322@ic.ac.uk>
2023-12-11 14:34:45 +0000

    Commit pipeline registers

---
+++ PipelineRegisters/ExecuteToMemory.sv
@@ -0,0 +1,26 @@
+module ExecuteToMemory #(
+    parameter   DATA_WIDTH = 32,
+                ADDRESS_WIDTH = 5
+)(
+    input  logic                       CLK, RegWriteE, MemWriteE,
+    input  logic [1:0]                 ResultSrcE,
+    input  logic [ADDRESS_WIDTH-1:0]   RdE,
+    input  logic [DATA_WIDTH-1:0]      ALUResultE, WriteDataE, PCPlus4E,
+
+    output  logic                      RegWriteM, MemWriteM,
+    output  logic [1:0]                ResultSrcM,
+    output  logic [ADDRESS_WIDTH-1:0]  RdM,
+    output  logic [DATA_WIDTH-1:0]     ALUResultM, WriteDataM, PCPlus4M
+    );
+
+always_ff @(posedge CLK) begin
+        RegWriteM <= RegWriteE;
+        MemWriteM <= MemWriteE;
+        ResultSrcM <= ResultSrcE;
+        RdM <= RdE;
+        ALUResultM <= ALUResultE;
+        WriteDataM <= WriteDataE;     
+        PCPlus4M <= PCPlus4E;
+end
+
+endmodule
---
+++ PipelineRegisters/FetchToDecode.sv
@@ -0,0 +1,25 @@
+module FetchToDecode #(
+    parameter   DATA_WIDTH = 32
+)(
+    input   logic                       CLK, EN, CLR,
+    input   logic [DATA_WIDTH-1:0]      InstrF, PCPlus4F, PCF,
+    output  logic [DATA_WIDTH-1:0]      InstrD, PCPlus4D, PCD
+);
+
+always_ff @(posedge CLK, posedge CLR) begin
+    if(~EN) begin
+        if(CLR) begin
+            InstrD <= 32'b0;
+            PCPlus4D <= 32'b0;
+            PCD <= 32'b0;
+        end
+        else begin
+            InstrD <= InstrF;
+            PCPlus4D <= PCPlus4F;
+            PCD <= PCF;
+        end
+    end
+
+end
+
+endmodule
